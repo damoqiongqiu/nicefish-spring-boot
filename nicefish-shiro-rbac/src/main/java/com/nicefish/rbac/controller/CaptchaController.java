4a11e995dd2e4acdb466b7eff0bb3c2497d85c18
大漠穷秋 <damoqiongqiu@126.com>
2023-07-20 17:31:09 +0800

    重构 CaptchaController

--- nicefish-shiro-rbac/src/main/java/com/nicefish/rbac/controller/CaptchaController.java
+++ nicefish-shiro-rbac/src/main/java/com/nicefish/rbac/controller/CaptchaController.java
@@ -22,6 +22,7 @@ import java.io.IOException;
 
 /**
  * 验证码
+ * TODO:这款验证码组件有点老，需要换一个更新一点的。
  * @author 大漠穷秋
  */
 @Api("Captcha Controller")
@@ -30,19 +31,28 @@ import java.io.IOException;
 public class CaptchaController {
     private static final Logger logger = LoggerFactory.getLogger(CaptchaController.class);
 
+    //文本型验证码
     @Resource(name = "captchaProducer")
     private Producer captchaProducer;
 
+    //数学计算型验证码
     @Resource(name = "captchaProducerMath")
     private Producer captchaProducerMath;
 
-    @ApiOperation("获取验证码，有两种类型：math或者char")
+    /**
+     * 获取验证码，有两种类型：math 或者 char 。
+     * 获取验证码的请求默认开启一个 HttpSession ，这个会话用来存储验证码结果，以便与用户输入的内容进行比对，
+     * 后续的 /login 和其它操作都会复用这个 HttpSession 。
+     * 在与 Shiro 整合时，这里获得的 Session 实例是被 Shiro 包装过的 ShiroHttpSession 类型。
+     * @param request
+     * @param response
+     * @return
+     */
+    @ApiOperation("获取验证码，有两种类型：math 或者 char")
     @GetMapping(value = "/captchaImage")
     public ModelAndView getKaptchaImage(HttpServletRequest request, HttpServletResponse response) {
         ServletOutputStream out = null;
         try {
-            //获取验证码的请求默认开启一个HttpSession，后续的login和其它操作都会复用这个HttpSession
-            //在与 Shiro 整合时，这里获得的 session 实例是被 Shiro 包装过的 ShiroHttpSession 类型。
             HttpSession session = request.getSession();
             response.setDateHeader("Expires", 0);
             response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
@@ -50,32 +60,33 @@ public class CaptchaController {
             response.setHeader("Pragma", "no-cache");
             response.setContentType("image/jpeg");
 
+            String capStr = null;   //验证码
+            String capResult = null;//结果，会被存在 session 中
+            BufferedImage bufferedImg = null;
             String type = request.getParameter("type");
-            String capStr = null;
-            String code = null;
-            BufferedImage bi = null;
             if ("math".equals(type)) {
                 String capText = captchaProducerMath.createText();
                 capStr = capText.substring(0, capText.lastIndexOf("@"));
-                code = capText.substring(capText.lastIndexOf("@") + 1);
-                bi = captchaProducerMath.createImage(capStr);
+                capResult = capText.substring(capText.lastIndexOf("@") + 1);
+                bufferedImg = captchaProducerMath.createImage(capStr);
             } else if ("char".equals(type)) {
-                capStr = code = captchaProducer.createText();
-                bi = captchaProducer.createImage(capStr);
+                capStr = capResult = captchaProducer.createText();
+                bufferedImg = captchaProducer.createImage(capStr);
             }
-            session.setAttribute(Constants.KAPTCHA_SESSION_KEY, code);
+            
+            session.setAttribute(Constants.KAPTCHA_SESSION_KEY, capResult);
             out = response.getOutputStream();
-            ImageIO.write(bi, "jpg", out);
+            ImageIO.write(bufferedImg, "jpg", out);
             out.flush();
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.debug(e.getMessage(),e);
         } finally {
             try {
                 if (out != null) {
                     out.close();
                 }
             } catch (IOException e) {
-                e.printStackTrace();
+                logger.debug(e.getMessage(),e);
             }
         }
         return null;
