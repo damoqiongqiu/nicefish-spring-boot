c93f76f938a2f66e0f441243363068dfccea9c0f
大漠穷秋 <damoqiongqiu@126.com>
2023-07-25 12:05:34 +0800

    package com.nicefish.rbac.shiro.filter;

--- nicefish-shiro-rbac/src/main/java/com/nicefish/rbac/shiro/filter/NiceFishCaptchaValidateFilter.java
+++ nicefish-shiro-rbac/src/main/java/com/nicefish/rbac/shiro/filter/NiceFishCaptchaValidateFilter.java
@@ -4,6 +4,8 @@ import com.google.code.kaptcha.Constants;
 import com.nicefish.rbac.constant.NiceFishAuthConstants;
 import com.nicefish.rbac.shiro.util.NiceFishSecurityUtils;
 import org.apache.shiro.web.filter.AccessControlFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -14,6 +16,8 @@ import javax.servlet.http.HttpServletRequest;
  * @author 大漠穷秋
  */
 public class NiceFishCaptchaValidateFilter extends AccessControlFilter {
+    private static final Logger logger = LoggerFactory.getLogger(NiceFishCaptchaValidateFilter.class);
+
     private boolean captchaEnabled = true;
 
     private String captchaType = "math";
@@ -28,14 +32,27 @@ public class NiceFishCaptchaValidateFilter extends AccessControlFilter {
     @Override
     protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
         if (!this.captchaEnabled)  return true;
-
         HttpServletRequest httpReq = (HttpServletRequest) request;
         String code = (String)NiceFishSecurityUtils.getSession().getAttribute(Constants.KAPTCHA_SESSION_KEY);
         String validateCode=httpReq.getParameter(NiceFishAuthConstants.CURRENT_VALIDATECODE);
+
+        //TODO:这里需要 fix 一下，处理 post 请求里面的 captcha 参数，否则会影响前端传递参数的方式。
+//        try{
+//            if("GET".equals(httpReq.getMethod())){
+//                validateCode=httpReq.getParameter(NiceFishAuthConstants.CURRENT_VALIDATECODE);
+//            }else if("POST".equals(httpReq.getMethod())){
+//                String jsonParams=httpReq.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+//                ObjectMapper mapper = new ObjectMapper();
+//                JsonNode actualObj = mapper.readTree(jsonParams);
+//                code= String.valueOf(actualObj.get("captcha"));
+//            }
+//        }catch (Exception e){
+//            logger.error(e.getMessage());
+//        }
+
         if (!code.equalsIgnoreCase(validateCode)) {
             return false;
         }
-
         return true;
     }
 
