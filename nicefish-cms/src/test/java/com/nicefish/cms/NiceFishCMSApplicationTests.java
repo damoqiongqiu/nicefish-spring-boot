08e2df7582a36086ab160c9d2a24a6067a6e076f
大漠穷秋 <damoqiongqiu@126.com>
2023-08-23 13:01:57 +0800

    清理 pom.xml

--- nicefish-cms/src/test/java/com/nicefish/cms/NiceFishCMSApplicationTests.java
+++ nicefish-cms/src/test/java/com/nicefish/cms/NiceFishCMSApplicationTests.java
@@ -1,16 +1,10 @@
 package com.nicefish.cms;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.Test;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class NiceFishCMSApplicationTests {
 
     @Test
     public void contextLoads() {
     }
-
 }
